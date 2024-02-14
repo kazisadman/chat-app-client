9033a18b8f202cb1f8363182dab7b22f9b147123
teethw <kazisadman01@gmail.com>
2024-02-14 23:41:56 +0600

    created logout button

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -13,7 +13,7 @@ const Chat = () => {
   const [messages, setMessages] = useState([]);
   const messageContainerRef = useRef(null);
 
-  console.log(offlinePeople,onlinePeople);
+  console.log(offlinePeople, onlinePeople);
 
   const { Id } = useContext(UserContextProvider);
 
@@ -125,42 +125,49 @@ const Chat = () => {
 
   return (
     <div className=" flex h-screen max-w-7xl mx-auto font-poppins">
-      <div className="bg-white w-1/3 pl-4 py-3">
-        <div className="font-bold text-blue-700 flex items-center gap-2 py-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 0 1-3.476.383.39.39 0 0 0-.297.17l-2.755 4.133a.75.75 0 0 1-1.248 0l-2.755-4.133a.39.39 0 0 0-.297-.17 48.9 48.9 0 0 1-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97ZM6.75 8.25a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H7.5Z"
-              clipRule="evenodd"
-            />
-          </svg>
-          LET&apos;Schat
+      <div className="bg-white w-1/3 pl-4 py-3 flex flex-col">
+        <div className="flex-grow">
+          <div className="font-bold text-blue-700 flex items-center gap-2 py-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 0 1-3.476.383.39.39 0 0 0-.297.17l-2.755 4.133a.75.75 0 0 1-1.248 0l-2.755-4.133a.39.39 0 0 0-.297-.17 48.9 48.9 0 0 1-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97ZM6.75 8.25a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H7.5Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            LET&apos;Schat
+          </div>
+          {Object.keys(onlinePeopleExcludingSelf).map((userId) => (
+            <Users
+              key={userId}
+              userId={userId}
+              online={true}
+              selected={selectedUserId}
+              onClick={() => selectedUser(userId)}
+              userName={onlinePeople[userId]}
+            ></Users>
+          ))}
+          {Object.keys(offlinePeople).map((userId) => (
+            <Users
+              key={userId}
+              userId={userId}
+              online={false}
+              selected={selectedUserId}
+              onClick={() => selectedUser(userId)}
+              userName={offlinePeople[userId]}
+            ></Users>
+          ))}
         </div>
-        {Object.keys(onlinePeopleExcludingSelf).map((userId) => (
-          <Users
-            key={userId}
-            userId={userId}
-            online={true}
-            selected={selectedUserId}
-            onClick={() => selectedUser(userId)}
-            userName={onlinePeople[userId]}
-          ></Users>
-        ))}
-        {Object.keys(offlinePeople).map((userId) => (
-          <Users
-            key={userId}
-            userId={userId}
-            online={false}
-            selected={selectedUserId}
-            onClick={() => selectedUser(userId)}
-            userName={offlinePeople[userId]}
-          ></Users>
-        ))}
+        <div className="mr-4">
+          <button className="bg-blue-600 text-white w-full  rounded-full">
+            logout
+          </button>
+        </div>
       </div>
       <div className=" overflow-y-scroll flex flex-col bg-blue-100 w-2/3 px-4 py-3">
         <div className="flex-grow">
