1aa37b98ba6352d36633ea0684c9bb355aadfcad
teethw <kazisadman01@gmail.com>
2024-02-15 22:32:02 +0600

    added login user name beside logout button

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -15,7 +15,7 @@ const Chat = () => {
 
   console.log(offlinePeople, onlinePeople);
 
-  const { Id } = useContext(UserContextProvider);
+  const { userName, Id } = useContext(UserContextProvider);
 
   useEffect(() => {
     connetToWs();
@@ -163,7 +163,23 @@ const Chat = () => {
             ></Users>
           ))}
         </div>
-        <div className="mr-4">
+        <div className="flex items-center gap-3 mr-4">
+          <div className="flex items-center  border-2 border-blue-500 p-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                clipRule="evenodd"
+              />
+            </svg>
+
+            {userName}
+          </div>
           <button className="bg-blue-600 text-white w-full  rounded-full">
             logout
           </button>
