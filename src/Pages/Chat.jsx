50aaef173f947df8e6ceaee21d21a6fc9c3652ee
teethw <kazisadman01@gmail.com>
2024-02-18 22:40:42 +0600

    added error message for login and reg form and moved user id from bottom to top

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -148,20 +148,38 @@ const Chat = () => {
     <div className=" flex h-screen max-w-7xl mx-auto font-poppins">
       <div className="bg-white w-1/3 pl-4 py-3 flex flex-col">
         <div className="flex-grow">
-          <div className="font-bold text-blue-700 flex items-center gap-2 py-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 0 1-3.476.383.39.39 0 0 0-.297.17l-2.755 4.133a.75.75 0 0 1-1.248 0l-2.755-4.133a.39.39 0 0 0-.297-.17 48.9 48.9 0 0 1-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97ZM6.75 8.25a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H7.5Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            LET&apos;Schat
+          <div className="flex justify-between items-center mr-4 mb-3">
+            <div className="font-bold text-blue-700 flex items-center gap-2 py-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 0 1-3.476.383.39.39 0 0 0-.297.17l-2.755 4.133a.75.75 0 0 1-1.248 0l-2.755-4.133a.39.39 0 0 0-.297-.17 48.9 48.9 0 0 1-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97ZM6.75 8.25a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H7.5Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              LET&apos;Schat
+            </div>
+            <div className="flex items-center  border-2 border-blue-500 p-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+
+              {userName}
+            </div>
           </div>
           {Object.keys(onlinePeopleExcludingSelf).map((userId) => (
             <Users
@@ -184,23 +202,7 @@ const Chat = () => {
             ></Users>
           ))}
         </div>
-        <div className="flex items-center gap-3 mr-4">
-          <div className="flex items-center  border-2 border-blue-500 p-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-                clipRule="evenodd"
-              />
-            </svg>
-
-            {userName}
-          </div>
+        <div className="gap-3 mr-4">
           <button
             onClick={handleLogOut}
             className="bg-blue-600 text-white w-full  rounded-full"
--- src/Pages/Register&Login.jsx
+++ src/Pages/Register&Login.jsx
@@ -6,25 +6,46 @@ const RegisterandLogin = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isLogin, setIsLogin] = useState(false);
+  const [error, setError] = useState("");
+
   const { setUserName: setLoggedUserName, setId } =
     useContext(UserContextProvider);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setError("");
+
     const url = isLogin ? "/login" : "/register";
-    axios
-      .post(url, { username, password })
-      .then((res) => {
-        setId(res.data.id);
-        setLoggedUserName(username);
-      })
-      .catch((err) => console.error(err));
+
+    if (password.length < 6) {
+      setError("Password should be ateast 6 characters");
+    } else {
+      axios
+        .post(url, { username, password })
+        .then((res) => {
+          console.log(res);
+          setId(res.data.id);
+          setLoggedUserName(username);
+        })
+        .catch((err) => {
+          if (err.response.status === 401) {
+            setError("Username or Password not matched");
+          }
+        });
+    }
   };
 
   return (
     <div className="flex flex-col justify-center items-center bg-blue-100 h-screen">
-      <div>Register Now</div>
-      <form onSubmit={handleSubmit} className="w-1/3 flex flex-col gap-4">
+      {isLogin ? (
+        <div className="text-blue-700 font-bold mb-6 text-3xl">Login</div>
+      ) : (
+        <div className="text-blue-700 font-bold mb-6 text-3xl">Register</div>
+      )}
+      <form
+        onSubmit={handleSubmit}
+        className="w-1/3 flex flex-col gap-4 bg-white p-6 rounded-lg mb-3"
+      >
         <input
           type="text"
           onChange={(e) => {
@@ -45,12 +66,16 @@ const RegisterandLogin = () => {
         <button className="bg-blue-800 text-white p-2">
           {isLogin ? "Login" : "Register"}
         </button>
+        {error && <div>{error}</div>}
       </form>
 
       {isLogin ? (
         <div>
           Don&apos;t have an account?
-          <button onClick={() => setIsLogin(false)}>Register</button> here
+          <button className="bg-blue" onClick={() => setIsLogin(false)}>
+            Register
+          </button>{" "}
+          here
         </div>
       ) : (
         <div>
